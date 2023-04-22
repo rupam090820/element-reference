7a6992168e63c2f0287cf85f72a13a51054083f1
rupam090820 <rupams441@gmail.com>
2023-04-22 22:53:15 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'elementref';
-}
+    numbers = [1, 2, 3, 4, 5];
+    oddNumbers = [1, 3, 5];
+    evenNumbers = [2, 4];
+    onlyOdd = true;
+    value = 5;
+  }
+
